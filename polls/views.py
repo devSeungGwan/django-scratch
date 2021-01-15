93741b54739af940f2b961e4ac04b5067ac2a935
devSeungGwan <yanhis97531@gmail.com>
2021-01-15 20:27:31 +0900

    ADD: Django tutorial #3 View

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,19 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, response
 from django.shortcuts import render
 
 def index(request):
     return HttpResponse("Hello World")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def result(request, question_id):
+    """
+    docstring
+    """
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're votting on question %s" % question_id)
